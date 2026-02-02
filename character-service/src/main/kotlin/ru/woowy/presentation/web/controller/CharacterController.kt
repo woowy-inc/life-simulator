233c29a72b6d9d59ae888f17d18ba91097649a9c
Denis <dnartysh@yandex.ru>
2026-02-02 20:06:47 +0300

    fix(GatewayService): add gateway route for services

--- character-service/src/main/kotlin/ru/woowy/presentation/web/controller/CharacterController.kt
+++ character-service/src/main/kotlin/ru/woowy/presentation/web/controller/CharacterController.kt
@@ -3,6 +3,7 @@ package ru.woowy.presentation.web.controller
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.woowy.character.Character
 import ru.woowy.domain.usecase.CharacterUseCase
@@ -10,12 +11,13 @@ import ru.woowy.domain.usecase.CharacterUseCase
 private const val BASE_ENDPOINT = "/character"
 
 @RestController
+@RequestMapping(BASE_ENDPOINT)
 internal class CharacterController(
     private val characterUseCase: CharacterUseCase,
 ) {
-    @PostMapping(BASE_ENDPOINT)
+    @PostMapping
     fun createCharacter(): ResponseEntity<Character> = ResponseEntity.ok(characterUseCase.createCharacter())
 
-    @GetMapping(BASE_ENDPOINT)
+    @GetMapping
     fun getCharacter(): ResponseEntity<Character> = ResponseEntity.ok(characterUseCase.getCharacter())
 }
