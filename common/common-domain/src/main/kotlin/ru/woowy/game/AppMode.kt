780797adcfea6bdf5e7120215168400d3d857127
Denis <dnartysh@yandex.ru>
2026-02-02 14:54:21 +0300

    feat(Config): add environment variables

--- common/common-domain/src/main/kotlin/ru/woowy/game/AppMode.kt
+++
@@ -1,6 +0,0 @@
-package ru.woowy.game
-
-enum class AppMode {
-    DEV,
-    PROD,
-}
--- time-service/src/main/kotlin/ru/woowy/application/config/AppProperties.kt
+++ time-service/src/main/kotlin/ru/woowy/application/config/AppProperties.kt
@@ -2,11 +2,9 @@ package ru.woowy.application.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.NestedConfigurationProperty
-import ru.woowy.game.AppMode
 
 @ConfigurationProperties(prefix = "app")
 internal data class AppProperties(
-    val mode: AppMode,
     val tickRate: Long,
     @NestedConfigurationProperty
     val jwt: JwtProperties = JwtProperties(),
