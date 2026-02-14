e151c5b9521fad81dfc7123f2515ce9951061e43
Denis <dnartysh@yandex.ru>
2026-02-14 16:29:58 +0300

    feat(AuthService): add app version

--- auth-service/src/main/kotlin/ru/woowy/presentation/web/controller/JwksController.kt
+++ auth-service/src/main/kotlin/ru/woowy/presentation/web/controller/JwksController.kt
@@ -1,11 +1,13 @@
 package ru.woowy.presentation.web.controller
 
+import io.swagger.v3.oas.annotations.Hidden
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.woowy.application.usecase.GetJwksUseCase
 
+@Hidden
 @RestController
 @RequestMapping("/.well-known")
 internal class JwksController(
