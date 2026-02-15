ef99aa60c91889a79076ca8f1266b3622c96587e
Denis <dnartysh@yandex.ru>
2026-02-15 17:57:13 +0300

    feat(NotificationService): init notification service

---
+++ notification-service/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    kotlin("jvm")
+
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+}
+
+group = "ru.woowy"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":common-domain"))
+    implementation(project(":common-events"))
+
+    implementation(libs.bundles.springboot.kafka.ecosystem)
+    implementation(libs.springboot.starter.eureka.client)
+
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+springBoot {
+    mainClass.set("ru.woowy.NotificationServiceApplicationKt")
+}
+
+tasks.named<Jar>("jar") {
+    enabled = false
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
