6d0702ee956175f7f9bdd87a16fe221d0c281198
Denis <dnartysh@yandex.ru>
2026-02-21 15:29:09 +0300

    feat(AuthService): add email verify event listener

--- auth-service/src/main/kotlin/ru/woowy/application/event/UserEventListener.kt
+++ auth-service/src/main/kotlin/ru/woowy/application/event/UserEventListener.kt
@@ -5,6 +5,7 @@ import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
+import ru.woowy.domain.model.EmailVerifyEvent
 import ru.woowy.domain.model.Event
 import ru.woowy.domain.model.UserRegisteredEvent
 import ru.woowy.game.KafkaTopic
@@ -13,22 +14,33 @@ import ru.woowy.game.KafkaTopic
 internal class UserEventListener(
     private val kafkaTemplate: KafkaTemplate<String, Event>,
 ) {
-    private val logger = LoggerFactory.getLogger(javaClass)
+    private val logger = LoggerFactory.getLogger(UserEventListener::class.java)
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun handleUserRegistered(event: UserRegisteredEvent) {
-        try {
-            val future = kafkaTemplate.send(KafkaTopic.USER_EVENTS, event.user.id, event)
+        sendEvent(KafkaTopic.USER_EVENTS, event.userId, event)
+    }
 
-            future.whenComplete { _, ex ->
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    fun handleEmailVerify(event: EmailVerifyEvent) {
+        sendEvent(KafkaTopic.USER_EVENTS, event.email, event)
+    }
+
+    private fun sendEvent(
+        topic: String,
+        key: String,
+        event: Event,
+    ) {
+        try {
+            kafkaTemplate.send(topic, key, event).whenComplete { _, ex ->
                 if (ex != null) {
-                    logger.error("Failed to send event", ex)
+                    logger.error("Failed to send event: $event", ex)
                 } else {
-                    logger.info("Event sent successfully!")
+                    logger.info("Event sent successfully: $event")
                 }
             }
         } catch (ex: Exception) {
-            logger.error("Listener exception", ex)
+            logger.error("Listener exception for event: $event", ex)
         }
     }
 }
