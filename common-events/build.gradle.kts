1b6aeb9c3075e9d7fe9111121c7a3c6a0da5485c
Denis <dnartysh@yandex.ru>
2026-02-23 13:35:33 +0300

    feat(CommonEvents): add UserRegisterRequestedEvent and UserRegisteredEvent

--- common-events/build.gradle.kts
+++ common-events/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ru.woowy"
-version = "0.0.1"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
--- common-events/src/main/kotlin/ru/woowy/domain/model/UserEvents.kt
+++ common-events/src/main/kotlin/ru/woowy/domain/model/UserEvents.kt
@@ -4,6 +4,17 @@ import kotlinx.serialization.Serializable
 import ru.woowy.security.UserRole
 
 @Serializable
+data class UserRegisterRequestedEvent(
+    override val eventId: String,
+    override val timestamp: Long,
+    val userId: String,
+    val username: String,
+    val email: String,
+    val firstName: String,
+    val key: String,
+) : Event
+
+@Serializable
 data class UserRegisteredEvent(
     override val eventId: String,
     override val timestamp: Long,
@@ -12,13 +23,4 @@ data class UserRegisteredEvent(
     val email: String,
     val firstName: String,
     val role: UserRole,
-) : Event
-
-@Serializable
-data class EmailVerifyEvent(
-    override val eventId: String,
-    override val timestamp: Long,
-    val firstName: String,
-    val email: String,
-    val token: String,
 ) : Event
