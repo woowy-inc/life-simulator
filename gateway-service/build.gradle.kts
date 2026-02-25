aab1990697694acb78855ab4e1c2c614ffc94411
Denis <dnartysh@yandex.ru>
2026-02-25 17:34:43 +0300

    chore(GatewayService): version up to 0.27.2

--- gateway-service/build.gradle.kts
+++ gateway-service/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ru.woowy"
-version = "0.2.0"
+version = "0.27.2"
 
 repositories {
     mavenCentral()
--- gateway-service/src/main/kotlin/ru/woowy/application/config/AppConfiguration.kt
+++ gateway-service/src/main/kotlin/ru/woowy/application/config/AppConfiguration.kt
@@ -7,6 +7,6 @@ import org.springframework.context.annotation.Configuration
 @EnableConfigurationProperties(AppProperties::class)
 internal class AppConfiguration {
     companion object {
-        const val APP_VERSION = "0.2.0"
+        const val APP_VERSION = "0.27.2"
     }
 }
