8a03612b97f59fac0e489450f587397630144544
Denis <dnartysh@yandex.ru>
2026-03-05 21:55:38 +0300

    fix(AuthService): remove Specification

--- world-service/src/main/kotlin/ru/woowy/infrastructure/persistence/jpa/JpaCityRepository.kt
+++ world-service/src/main/kotlin/ru/woowy/infrastructure/persistence/jpa/JpaCityRepository.kt
@@ -1,15 +1,12 @@
 package ru.woowy.infrastructure.persistence.jpa
 
-import org.springframework.data.jpa.domain.Specification
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import ru.woowy.domain.model.CityId
 import ru.woowy.infrastructure.persistence.entity.CityEntity
 import java.util.Optional
 
-interface JpaCityRepository :
-    JpaRepository<CityEntity, CityId>,
-    Specification<CityEntity> {
+interface JpaCityRepository : JpaRepository<CityEntity, CityId> {
     @EntityGraph(attributePaths = ["region", "region.nameCase", "timezone", "nameCase"])
     override fun findAll(): List<CityEntity>
 
