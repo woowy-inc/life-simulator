535a0bc87b4212a4de3f261d42468c163abe1c06
Denis <dnartysh@yandex.ru>
2026-03-09 18:02:35 +0300

    chore(GatewayService): replicate Service object

--- gateway-service/src/main/kotlin/ru/woowy/infrastructure/config/RouteConfig.kt
+++ gateway-service/src/main/kotlin/ru/woowy/infrastructure/config/RouteConfig.kt
@@ -15,27 +15,27 @@ class RouteConfig {
     @Bean
     fun gatewayRoutes(): RouterFunction<ServerResponse> = route("auth-jwks")
         .GET("/.well-known/**", http())
-        .filter(lb(Service.AUTH_SERVICE.id))
+        .filter(lb(Service.AUTH_SERVICE))
         .build()
         .and(
             route("auth-route")
                 .route(path("/user/**"), http())
-                .filter(lb(Service.AUTH_SERVICE.id))
+                .filter(lb(Service.AUTH_SERVICE))
                 .build(),
         ).and(
             route("time-route")
                 .route(path("/time/**"), http())
-                .filter(lb(Service.TIME_SERVICE.id))
+                .filter(lb(Service.TIME_SERVICE))
                 .build(),
         ).and(
             route("character-route")
                 .route(path("/character/**"), http())
-                .filter(lb(Service.CHARACTER_SERVICE.id))
+                .filter(lb(Service.CHARACTER_SERVICE))
                 .build(),
         ).and(
             route("world-route")
                 .route(path("/world/**"), http())
-                .filter(lb(Service.WORLD_SERVICE.id))
+                .filter(lb(Service.WORLD_SERVICE))
                 .build(),
         ).and(
             route("not-found")
