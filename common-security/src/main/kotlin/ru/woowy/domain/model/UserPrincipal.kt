66b4a4c3ba1f7d360b645197de3a8f9d3ec7134e
Denis <dnartysh@yandex.ru>
2026-03-09 18:50:44 +0300

    fix(CommonSecurity): fix id import

--- common-security/src/main/kotlin/ru/woowy/domain/model/UserPrincipal.kt
+++ common-security/src/main/kotlin/ru/woowy/domain/model/UserPrincipal.kt
@@ -3,7 +3,7 @@ package ru.woowy.domain.model
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
-import ru.woowy.security.UserId
+import ru.woowy.id.UserId
 import ru.woowy.security.UserRole
 
 data class UserPrincipal(
