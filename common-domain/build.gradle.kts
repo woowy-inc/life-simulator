822202da6d7587129c1d471b479d08870fadb2e5
Denis <dnartysh@yandex.ru>
2026-03-10 15:08:35 +0300

    chore(CommonDomain): add character event topic

--- common-domain/build.gradle.kts
+++ common-domain/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ru.woowy"
-version = "0.14.0"
+version = "0.15.0"
 
 repositories {
     mavenCentral()
--- common-domain/src/main/kotlin/ru/woowy/messaging/KafkaTopic.kt
+++ common-domain/src/main/kotlin/ru/woowy/messaging/KafkaTopic.kt
@@ -2,5 +2,6 @@ package ru.woowy.messaging
 
 object KafkaTopic {
     const val USER_EVENTS = "user-events"
+    const val CHARACTER_EVENTS = "character-events"
     const val WORLD_TICK = "world-tick-topic"
 }
