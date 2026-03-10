32fdc39812968734f404558a5a4bd32e1fbdf548
Denis <dnartysh@yandex.ru>
2026-03-10 15:24:51 +0300

    feat(CommonDomain): add world events

--- common-domain/src/main/kotlin/ru/woowy/messaging/KafkaTopic.kt
+++ common-domain/src/main/kotlin/ru/woowy/messaging/KafkaTopic.kt
@@ -3,5 +3,6 @@ package ru.woowy.messaging
 object KafkaTopic {
     const val USER_EVENTS = "user-events"
     const val CHARACTER_EVENTS = "character-events"
+    const val WORLD_EVENTS = "world-events"
     const val WORLD_TICK = "world-tick-topic"
 }
