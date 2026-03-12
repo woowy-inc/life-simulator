88fce3c50afdced503a90ed5605650ddebcb15aa
Denis <dnartysh@yandex.ru>
2026-03-12 20:29:58 +0300

    chore(Gradle): remove time service

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,11 +18,11 @@ plugins {
 
 include(":common-domain")
 include(":common-events")
-include(":time-service")
-include(":world-service")
-include(":character-service")
-include(":auth-service")
+include(":common-security")
+
 include(":eureka-service")
 include(":gateway-service")
+include(":auth-service")
+include(":character-service")
+include(":world-service")
 include(":notification-service")
-include(":common-security")
