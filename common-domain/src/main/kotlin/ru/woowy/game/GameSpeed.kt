414ad6fcefd2f885f0a5cd056c93e3f23fef79a4
Denis <dnartysh@yandex.ru>
2026-03-12 22:48:34 +0300

    fix(CommonDomain): fix real time option

--- common-domain/src/main/kotlin/ru/woowy/game/GameSpeed.kt
+++ common-domain/src/main/kotlin/ru/woowy/game/GameSpeed.kt
@@ -3,7 +3,7 @@ package ru.woowy.game
 enum class GameSpeed(
     val gameMinutesPerTick: Int,
 ) {
-    REAL_TIME(gameMinutesPerTick = 3),
+    REAL_TIME(gameMinutesPerTick = GameConfig.TICK_INTERVAL_SECONDS),
     ACCELERATED(gameMinutesPerTick = 72),
     FAST(gameMinutesPerTick = 504),
 }
