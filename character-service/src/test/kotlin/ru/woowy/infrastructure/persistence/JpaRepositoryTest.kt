27297fb9ebc3b02a63eba90c5ade46367c9aa712
Denis <dnartysh@yandex.ru>
2026-03-12 22:49:58 +0300

    chore(CharacterService): add tests

---
+++ character-service/src/test/kotlin/ru/woowy/infrastructure/persistence/JpaRepositoryTest.kt
@@ -0,0 +1,24 @@
+package ru.woowy.infrastructure.persistence
+
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.springframework.test.context.TestConstructor
+import org.testcontainers.junit.jupiter.Testcontainers
+
+@DataJpaTest
+@Testcontainers
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class JpaRepositoryTest {
+    companion object {
+        @JvmStatic
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", TestPostgresContainer.instance::getJdbcUrl)
+            registry.add("spring.datasource.username", TestPostgresContainer.instance::getUsername)
+            registry.add("spring.datasource.password", TestPostgresContainer.instance::getPassword)
+        }
+    }
+}
