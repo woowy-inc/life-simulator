9333f0a5e1318c8f8e7805b94b0a43c05c310fa4
Denis <dnartysh@yandex.ru>
2026-03-12 20:44:24 +0300

    feat(EngineService): init engine service

---
+++ engine-service/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    kotlin("jvm")
+
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.kotlin.jpa)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+}
+
+group = "ru.woowy"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":common-domain"))
+    implementation(project(":common-events"))
+    implementation(project(":common-security"))
+
+    implementation(libs.bundles.kotlinx.ecosystem)
+
+    implementation(libs.springboot.kafka.starter)
+    implementation(libs.springboot.starter.data.jpa)
+    implementation(libs.springboot.starter.data.redis)
+    implementation(libs.springboot.starter.eureka.client)
+    implementation(libs.springboot.starter.websocket)
+    implementation(libs.springboot.kafka)
+
+    runtimeOnly(libs.postgresql.jdbc)
+
+    testImplementation(libs.bundles.test.jpa.ecosystem)
+    testImplementation(libs.mockk)
+    testImplementation(testFixtures(project(":common-domain")))
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+springBoot {
+    buildInfo()
+    mainClass.set("ru.woowy.EngineServiceApplicationKt")
+}
+
+tasks.named<Jar>("jar") {
+    enabled = false
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
