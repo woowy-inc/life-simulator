280ce135251f2d29702a77b66ad800e2b29bf46c
Denis <dnartysh@yandex.ru>
2026-03-13 17:51:49 +0300

    fix(CharacterService): remove ResourceServerSecurityConfiguration import

--- character-service/src/main/kotlin/ru/woowy/infrastructure/config/SecurityConfig.kt
+++ character-service/src/main/kotlin/ru/woowy/infrastructure/config/SecurityConfig.kt
@@ -2,7 +2,6 @@ package ru.woowy.infrastructure.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
-import ru.woowy.application.config.ResourceServerSecurityConfiguration
 
 @Configuration
 @Import(ResourceServerSecurityConfiguration::class)
