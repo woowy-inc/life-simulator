bdf250898aba83da5c324afa1fdddee3282f15eb
Denis <dnartysh@yandex.ru>
2026-03-16 13:50:31 +0300

    chore(CommonSpring): add coroutine dispatcher to scope lifecycle

--- common-spring/src/main/kotlin/ru/woowy/infrastructure/lifecycle/CoroutineScopeLifecycle.kt
+++ common-spring/src/main/kotlin/ru/woowy/infrastructure/lifecycle/CoroutineScopeLifecycle.kt
@@ -1,5 +1,6 @@
 package ru.woowy.infrastructure.lifecycle
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -12,6 +13,7 @@ import kotlin.coroutines.CoroutineContext
 
 abstract class CoroutineScopeLifecycle(
     private val name: String,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.Default,
 ) : SmartLifecycle,
     CoroutineScope {
     private val logger = classLogger()
@@ -24,13 +26,15 @@ abstract class CoroutineScopeLifecycle(
         if (!scope.isActive) {
             scope = createScope()
         } else {
-            logger.warn("Scope[$name] is already active")
+            logger.warn("Lifecycle scope[$name] is already started...")
         }
     }
 
-    override fun stop() = scope.cancel()
+    override fun stop() = scope.cancel().also { logger.info("Lifecycle scope[$name] successfully stopped...") }
 
     override fun isRunning(): Boolean = scope.isActive
 
-    private fun createScope() = CoroutineScope(Dispatchers.Default + SupervisorJob() + CoroutineName(name))
+    private fun createScope() = CoroutineScope(dispatcher + SupervisorJob() + CoroutineName(name)).also {
+        logger.info("Lifecycle scope[$name] successfully created...")
+    }
 }
