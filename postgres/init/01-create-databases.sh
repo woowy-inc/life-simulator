970d36ea4abf829b8b731d4b4ba7be07a5de1bc6
Denis <dnartysh@yandex.ru>
2026-03-31 13:46:09 +0300

    fix(Postgresql): init databases

--- postgres/init/01-create-databases.sh
+++ postgres/init/01-create-databases.sh
@@ -2,11 +2,18 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    -- Auth Service Database
     CREATE DATABASE lifesim_auth;
     GRANT ALL PRIVILEGES ON DATABASE lifesim_auth TO $POSTGRES_USER;
 
-    -- World Service Database
+    CREATE DATABASE lifesim_need;
+    GRANT ALL PRIVILEGES ON DATABASE lifesim_need TO $POSTGRES_USER;
+
+    CREATE DATABASE lifesim_character;
+    GRANT ALL PRIVILEGES ON DATABASE lifesim_character TO $POSTGRES_USER;
+
+    CREATE DATABASE lifesim_engine;
+    GRANT ALL PRIVILEGES ON DATABASE lifesim_engine TO $POSTGRES_USER;
+
     CREATE DATABASE lifesim_world;
     GRANT ALL PRIVILEGES ON DATABASE lifesim_world TO $POSTGRES_USER;
 EOSQL
