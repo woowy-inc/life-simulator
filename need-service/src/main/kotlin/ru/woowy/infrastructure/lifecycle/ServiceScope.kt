51560ad34d9615fae9d8f7dec40bee3c77b49fad
Denis <dnartysh@yandex.ru>
2026-04-01 16:57:22 +0300

    feat(NeedService): move options to main constructor

--- need-service/src/main/kotlin/ru/woowy/infrastructure/lifecycle/ServiceScope.kt
+++ need-service/src/main/kotlin/ru/woowy/infrastructure/lifecycle/ServiceScope.kt
@@ -1,6 +1,11 @@
 package ru.woowy.infrastructure.lifecycle
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import org.springframework.stereotype.Component
 
 @Component
-class ServiceScope : CoroutineScopeLifecycle("service-worker")
+class ServiceScope(
+    name: String = "service-worker",
+    dispatcher: CoroutineDispatcher = Dispatchers.IO,
+) : CoroutineScopeLifecycle(name, dispatcher)
