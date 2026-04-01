7c0068a39d4260f2d3ddd9ac878b32249ad7d31c
Denis <dnartysh@yandex.ru>
2026-04-01 21:49:31 +0300

    feat(CommonDomain): add need service

--- common-domain/src/main/kotlin/ru/woowy/security/Service.kt
+++ common-domain/src/main/kotlin/ru/woowy/security/Service.kt
@@ -5,8 +5,8 @@ typealias ServiceId = String
 object Service {
     const val GATEWAY_SERVICE = "gateway-service"
     const val AUTH_SERVICE = "auth-service"
-    const val TIME_SERVICE = "time-service"
     const val CHARACTER_SERVICE = "character-service"
     const val WORLD_SERVICE = "world-service"
     const val ENGINE_SERVICE = "engine-service"
+    const val NEED_SERVICE = "need-service"
 }
