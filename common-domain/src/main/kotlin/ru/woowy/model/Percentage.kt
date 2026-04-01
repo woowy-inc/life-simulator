6fe5f091de69738f3dcbc736ebba2c41b2017396
Denis <dnartysh@yandex.ru>
2026-04-01 14:16:55 +0300

    feat(CommonDomain): add scale rounding to Percentage

--- common-domain/src/main/kotlin/ru/woowy/model/Percentage.kt
+++ common-domain/src/main/kotlin/ru/woowy/model/Percentage.kt
@@ -1,61 +1,64 @@
 package ru.woowy.model
 
 import java.math.BigDecimal
+import java.math.RoundingMode
 
 @JvmInline
 value class Percentage private constructor(
     val value: BigDecimal,
 ) {
     companion object {
+        private const val SCALE = 2
         private val MIN = BigDecimal(0)
         private val MAX = BigDecimal(100)
 
-        fun of(value: BigDecimal): Percentage = Percentage(value.coerceIn(MIN, MAX))
+        fun of(value: BigDecimal): Percentage =
+            Percentage(value.setScale(SCALE, RoundingMode.HALF_UP).coerceIn(MIN, MAX))
 
         fun max(): Percentage = Percentage(MAX)
 
         fun min(): Percentage = Percentage(MIN)
     }
 
-    operator fun plus(other: BigDecimal): Percentage = Percentage((value + other).coerceIn(MIN, MAX))
+    operator fun plus(other: BigDecimal) = of((value + other))
 
-    operator fun plus(other: Int): Percentage = plus(other.toBigDecimal())
+    operator fun plus(other: Int) = plus(other.toBigDecimal())
 
-    operator fun plus(other: Long): Percentage = plus(other.toBigDecimal())
+    operator fun plus(other: Long) = plus(other.toBigDecimal())
 
-    operator fun plus(other: Double): Percentage = plus(other.toBigDecimal())
+    operator fun plus(other: Double) = plus(other.toBigDecimal())
 
-    operator fun plus(other: Percentage): Percentage = plus(other.value)
+    operator fun plus(other: Percentage) = plus(other.value)
 
-    operator fun minus(other: BigDecimal): Percentage = Percentage((value - other).coerceIn(MIN, MAX))
+    operator fun minus(other: BigDecimal) = of((value - other))
 
     operator fun minus(other: Int): Percentage = minus(other.toBigDecimal())
 
-    operator fun minus(other: Long): Percentage = minus(other.toBigDecimal())
+    operator fun minus(other: Long) = minus(other.toBigDecimal())
 
-    operator fun minus(other: Double): Percentage = minus(other.toBigDecimal())
+    operator fun minus(other: Double) = minus(other.toBigDecimal())
 
-    operator fun minus(other: Percentage): Percentage = minus(other.value)
+    operator fun minus(other: Percentage) = minus(other.value)
 
-    operator fun times(other: BigDecimal): Percentage = Percentage((value * other).coerceIn(MIN, MAX))
+    operator fun times(other: BigDecimal) = of((value * other))
 
-    operator fun times(other: Int): Percentage = times(other.toBigDecimal())
+    operator fun times(other: Int) = times(other.toBigDecimal())
 
-    operator fun times(other: Long): Percentage = times(other.toBigDecimal())
+    operator fun times(other: Long) = times(other.toBigDecimal())
 
-    operator fun times(other: Double): Percentage = times(other.toBigDecimal())
+    operator fun times(other: Double) = times(other.toBigDecimal())
 
-    operator fun times(other: Percentage): Percentage = times(other.value)
+    operator fun times(other: Percentage) = times(other.value)
 
-    operator fun div(other: BigDecimal): Percentage = Percentage((value / other).coerceIn(MIN, MAX))
+    operator fun div(other: BigDecimal) = of((value / other))
 
-    operator fun div(other: Int): Percentage = div(other.toBigDecimal())
+    operator fun div(other: Int) = div(other.toBigDecimal())
 
-    operator fun div(other: Long): Percentage = div(other.toBigDecimal())
+    operator fun div(other: Long) = div(other.toBigDecimal())
 
-    operator fun div(other: Double): Percentage = div(other.toBigDecimal())
+    operator fun div(other: Double) = div(other.toBigDecimal())
 
-    operator fun div(other: Percentage): Percentage = div(other.value)
+    operator fun div(other: Percentage) = div(other.value)
 
     fun toDouble(): Double = this.value.toDouble()
 }
