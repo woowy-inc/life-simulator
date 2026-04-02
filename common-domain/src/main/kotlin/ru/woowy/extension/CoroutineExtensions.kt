34620486d88aa48c96838ccf10c0f8aad10e4f85
Denis <dnartysh@yandex.ru>
2026-04-02 22:15:00 +0300

    feat(CommonDomain): add async catching scope extension

--- common-domain/src/main/kotlin/ru/woowy/extension/CoroutineExtensions.kt
+++ common-domain/src/main/kotlin/ru/woowy/extension/CoroutineExtensions.kt
@@ -1,7 +1,36 @@
 package ru.woowy.extension
 
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import org.slf4j.Logger
 
-fun Job.invokeOnError(block: (Throwable) -> Unit) = invokeOnCompletion {
+fun Job.invokeOnFailure(block: (Throwable) -> Unit) = invokeOnCompletion {
     if (it != null) block(it)
+}
+
+fun <T> CoroutineScope.asyncCatching(
+    context: CoroutineContext = EmptyCoroutineContext,
+    start: CoroutineStart = CoroutineStart.DEFAULT,
+    block: suspend CoroutineScope.() -> T
+): Deferred<Result<T>> {
+    return async(context, start) { runCatching { block() } }
+}
+
+fun <T> Result<T>.getOrNullLogging(logger: Logger? = null): T? {
+    val ex = exceptionOrNull()
+
+    ex?.let {
+        if (logger == null) {
+            println(this.isFailure)
+        } else {
+            logger.error(it.message)
+        }
+    }
+
+    return getOrNull()
 }
